98b7cbc9bd5a385f97f8eaf30a371aeff7827c7e
Jon Sayers <jon@sayers.cloud>
2021-03-18 13:43:43 +0000

    Loops n Threads

--- MistyDemo/Classes/MDevice.cs
+++ MistyDemo/Classes/MDevice.cs
@@ -11,7 +11,7 @@ namespace MistyDemo.Classes
 
         public MDevice()
         {
-            Interval = 20;
+            Interval = 5;
         }
     }
 }
--- MistyDemo/Classes/MHubService.cs
+++ MistyDemo/Classes/MHubService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,19 @@ namespace MistyDemo.Classes
 {
     public class MHubService : BackgroundService
     {
+        private static DeviceClient _client;
+        private static string _conString = "HostName=wezmondo.azure-devices.net;DeviceId=Misty01;SharedAccessKey=8/gRFZW5LkhahVM41Dbh9UH7c0o1B0ttOtz0yIvrqHU=";
+
+        private Thread _sendThread { get; set; }
+
+        private MQueue _queue;
+        private MDevice _device;
+
+        public MHubService(MQueue queue, MDevice device)
+        {
+            _queue = queue;
+            _device = device;
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -20,7 +36,24 @@ namespace MistyDemo.Classes
 
         private async Task Setup()
         {
-            Console.WriteLine("Hub Ready...");
+            Console.WriteLine("Starting Hub...");
+
+            _client = DeviceClient.CreateFromConnectionString(_conString, TransportType.Mqtt);
+
+            _sendThread = new Thread(SendLoop);
+            _sendThread.Start();
+        }
+
+        private async void SendLoop()
+        {
+            while (true)
+            {
+                MReading toSend = _queue.GetQueued();
+                if (toSend != null)
+                {
+                    Console.WriteLine("Sent: " + JsonConvert.SerializeObject(toSend));
+                }
+            }
         }
 
     }
