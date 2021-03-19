55519409176c5067a53ee9d1be83772048681124
Jon Sayers <jon@sayers.cloud>
2021-03-19 13:26:01 +0000

    rules engine

--- MistyDemo/Classes/MDevice.cs
+++ MistyDemo/Classes/MDevice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MistyDemo.Classes
@@ -18,12 +19,48 @@ namespace MistyDemo.Classes
 
         public int CheckReading(MReading reading)
         {
-            foreach(MAlert alert in Alerts)
+            Regex rx = new Regex(@"\[.+?\]");
+            
+            foreach (MAlert alert in Alerts)
             {
-                // reading.Payload.Where(x => x.Key == "").Select(x => x.Value);
+                string toEval = alert.Rule;
+
+                List<Match> ruleParams = rx.Matches(toEval).ToList();
+
+                bool paramsApplied = true;
+
+                foreach (Match m in ruleParams)
+                {
+
+                    string v = reading.Payload.Where(x => ("[" + x.Key + "]").ToUpper() == m.ToString().ToUpper()).Select(x => x.Value).FirstOrDefault();
+
+                    if (v != null)
+                    {
+                        toEval = toEval.Replace(m.ToString(), v);
+                    }
+                    else
+                    {
+                        paramsApplied = false;
+                    }
+                }
+
+                if (paramsApplied)
+                {
+                    System.Data.DataTable table = new System.Data.DataTable();
+                    table.Columns.Add("expression", string.Empty.GetType(), toEval);
+                    System.Data.DataRow row = table.NewRow();
+                    table.Rows.Add(row);
+
+                    alert.Eval = bool.Parse((string)row["expression"]);
+                }
+                else
+                {
+                    alert.Eval = false;
+                }
+
             }
 
-            return 0;
+            return Alerts.Where(x => x.Eval).Count();
         }
     }
 
